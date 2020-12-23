b5f546b693431c324bd736f1081c142449057a90
Matt Severini <matthew.severini@gmail.com>
2020-12-23 08:46:42 -0700

    run at 1 mountain

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@
 set :chronic_options, hours24: true
 
 # By default this would run the job every day at 3am
-every 1.day, at: '1:00' do
+every 1.day, at: '20:00' do
   runner "ImportVotersJob.perform_now"
   runner "UpdateVotersJob.perform_now"
 end
