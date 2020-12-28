f4626f263950b667c2704ba48149a716ad1907c5
Matt Severini <matthew.severini@gmail.com>
2020-12-27 19:19:41 -0700

    schedule for midnight

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,7 @@ set :chronic_options, hours24: true
 env :PATH, ENV['PATH']
 
 # By default this would run the job every day at 3am
-every 1.day, at: '01:00' do
+every 1.day, at: '08:00' do
   runner "ImportVotersJob.perform_now"
   runner "UpdateVotersJob.perform_now"
 end
