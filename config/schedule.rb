5d0509934dd5dabe37f502bc5a6bfdf7f300c453
Matt Severini <matthew.severini@gmail.com>
2020-12-27 19:15:29 -0700

    set the path variable

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 set :chronic_options, hours24: true
 
+env :PATH, ENV['PATH']
+
 # By default this would run the job every day at 3am
 every 1.day, at: '01:00' do
   runner "ImportVotersJob.perform_now"
