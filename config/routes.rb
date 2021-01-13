47eaf58faf2b74c45b57b183a4b3836db6c95d5d
Matt Severini <matthew.severini@gmail.com>
2021-01-12 19:29:01 -0700

    enable api updates to voters

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   resources :healthz, only: [:index]
-  resources :voters, only: [:index, :show] do
+  resources :voters, only: [:index, :show, :update] do
     put 'import', on: :collection
   end
 end
